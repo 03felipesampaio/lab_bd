5afefe3338488f95dc1a09b2d53555dd32f69163
Felipe Sampaio <03.felipesampaio@gmail.com>
2022-07-03 00:44:00 -0300

    Adiciona nome as colunas

--- queries/Projeto Final/overviewConstructor.sql
+++ queries/Projeto Final/overviewConstructor.sql
@@ -1,3 +1,3 @@
-SELECT COUNT(*) FROM results WHERE position=1 AND constructorid=1;
-SELECT COUNT(DISTINCT(driverid)) FROM results WHERE constructorid=1;
-SELECT MIN(RA.year), MAX(RA.year) FROM results JOIN races RA USING (raceid)	WHERE constructorid=1;
+SELECT COUNT(*) as qtd_vitorias FROM results WHERE position=1 AND constructorid=1;
+SELECT COUNT(DISTINCT(driverid)) qtd_pilotos FROM results WHERE constructorid=1;
+SELECT MIN(RA.year) prim_ano, MAX(RA.year) ult_ano FROM results JOIN races RA USING (raceid)	WHERE constructorid=1;
