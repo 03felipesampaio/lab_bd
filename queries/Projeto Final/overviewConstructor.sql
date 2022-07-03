a09e318ccd5d425e9fdc49f6537c4b0795347c4e
Felipe Sampaio <03.felipesampaio@gmail.com>
2022-07-02 22:37:46 -0300

    Retirei aspas duplas dos campos

--- queries/Projeto Final/overviewConstructor.sql
+++ queries/Projeto Final/overviewConstructor.sql
@@ -1,3 +1,3 @@
-SELECT COUNT(*) FROM results WHERE "position"=1 AND constructorid=1;
+SELECT COUNT(*) FROM results WHERE position=1 AND constructorid=1;
 SELECT COUNT(DISTINCT(driverid)) FROM results WHERE constructorid=1;
-SELECT MIN(RA."year"), MAX(RA."year") FROM results JOIN races RA USING (raceid)	WHERE constructorid=1;
+SELECT MIN(RA.year), MAX(RA.year) FROM results JOIN races RA USING (raceid)	WHERE constructorid=1;
